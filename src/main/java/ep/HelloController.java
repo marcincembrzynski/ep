776d1c86d83309209be82c2fb7771c6bc050277a
marcincembrzynski <marcin@inandi.com>
2018-11-10 08:30:45 +0000

    initial commit

---
+++ src/main/java/ep/HelloController.java
@@ -0,0 +1,13 @@
+package ep;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HelloController {
+
+    @RequestMapping("/hello")
+    public String get(){
+        return "hello";
+    }
+}
